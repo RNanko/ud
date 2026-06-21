3f6141aea2130272244bf4c734ba03078c0c1459
Roman Naumenko <rnaumenko323@gmail.com>
2026-06-21 18:17:27 +0200

    page motivator size fix

--- app/(main)/account/motivator/page.tsx
+++ app/(main)/account/motivator/page.tsx
@@ -7,12 +7,15 @@ export default function Cards() {
   return (
     <section>
       <h2 className="text-center mb-10">Choose Your Path</h2>
-      <div className="flex gap-10 px-10">
-        <Link href="/account/motivator/go-hard" className="w-1/2 flex">
+      <div className="flex flex-col md:flex-row gap-10 px-10">
+        <Link href="/account/motivator/go-hard" className="w-full md:w-1/2">
           <CardM videoSource="/motivation/David G.mp4" text="Go Hard" />
         </Link>
 
-        <Link href="/account/motivator/go-with-wisdom" className="w-1/2">
+        <Link
+          href="/account/motivator/go-with-wisdom"
+          className="w-full md:w-1/2"
+        >
           <CardM
             videoSource="/motivation/Marcus Aurelius.mp4"
             text="Go with Wisdom"
